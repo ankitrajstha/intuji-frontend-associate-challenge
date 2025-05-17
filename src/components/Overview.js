9efdf9565acf260bacfed0ef96fd3921bc0ff256
Ankit Shrestha <ankitrajshrestha078@gmail.com>
2025-05-17 15:17:28 +0545

    feat(overview):dynamically add active state and contents in overwiew container

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,3 +1,38 @@
+const overviewCardItems = [
+  {
+    icon: "empty-wallet.svg",
+    heading: "Your Balance",
+    subHeading: "15 % compared with last month",
+    subHeadingIcon: "export.svg",
+    balance: "$ 28,891.138",
+    active: true,
+  },
+  {
+    icon: "save-2.svg",
+    heading: "Saving",
+    subHeading: "10 % compared with last month",
+    subHeadingIcon: "export-1.svg",
+    balance: "$ 1,050.44",
+    active: false,
+  },
+  {
+    icon: "direct-up.svg",
+    heading: "Saving",
+    subHeading: "2 % compared with last month",
+    subHeadingIcon: "export-3.svg",
+    balance: "$ 200.31",
+    active: false,
+  },
+  {
+    icon: "direct-down.svg",
+    heading: "Saving",
+    subHeading: "8 % compared with last month",
+    subHeadingIcon: "export-4.svg",
+    balance: "$ 21,121.0",
+    active: false,
+  },
+];
+
 export default function Overview() {
   const overViewContainer = document.createElement("div");
   overViewContainer.className = "overview-container";
@@ -9,77 +44,85 @@ export default function Overview() {
   const overViewCardContainer = document.createElement("div");
   overViewCardContainer.className = "overview-card-container";
 
-  const card = document.createElement("div");
-  card.className = "card";
+  function createCard(overviewCardItem) {
+    const card = document.createElement("div");
+    card.className = `card ${overviewCardItem.active ? "active" : ""}`;
 
-  const cardContainer = document.createElement("div");
-  cardContainer.className = "card-container";
+    const cardContainer = document.createElement("div");
+    cardContainer.className = "card-container";
 
-  card.appendChild(cardContainer);
+    const cardTop = document.createElement("div");
+    cardTop.className = "card-top";
 
-  const cardTop = document.createElement("div");
-  cardTop.className = "card-top";
+    const cardIconContainer = document.createElement("div");
+    cardIconContainer.className = `card-icon-container ${
+      overviewCardItem.active ? "active" : ""
+    }`;
 
-  const cardIconContainer = document.createElement("div");
-  cardIconContainer.className = "card-icon-container";
-  cardTop.appendChild(cardIconContainer);
+    const cardIcon = document.createElement("div");
+    cardIcon.className = `card-icon ${overviewCardItem.active ? "active" : ""}`;
+    cardIcon.style.backgroundImage = `url(${overviewCardItem.icon})`;
+    cardIconContainer.appendChild(cardIcon);
 
-  const cardIcon = document.createElement("img");
-  cardIcon.className = "card-icon";
-  cardIcon.src = "empty-wallet.svg";
-  cardIcon.alt = "empty-wallet-logo";
+    const cardHeadingContainer = document.createElement("div");
+    cardHeadingContainer.className = "card-heading-container";
 
-  const cardHeadingContainer = document.createElement("div");
-  cardHeadingContainer.className = "card-heading-container";
-  cardTop.appendChild(cardHeadingContainer);
+    const cardHeading = document.createElement("h3");
+    cardHeading.className = `${overviewCardItem.active ? "active" : ""}`;
+    cardHeading.innerText = `${overviewCardItem.heading}`;
 
-  const cardHeading = document.createElement("h3");
-  cardHeading.innerText = "Your Balance";
+    const subHeadingContainer = document.createElement("div");
+    subHeadingContainer.className = "subheading-container";
 
-  cardHeadingContainer.appendChild(cardHeading);
+    const subHeadingIcon = document.createElement("div");
+    subHeadingIcon.className = "subheading-icon";
+    subHeadingIcon.style.backgroundImage = `url(${overviewCardItem.subHeadingIcon})`;
 
-  const subHeadingContainer = document.createElement("div");
-  subHeadingContainer.className = "subheading-container";
+    const subHeading = document.createElement("p");
+    subHeading.className = `${overviewCardItem.active ? "active" : ""}`;
+    subHeading.textContent = `${overviewCardItem.subHeading}`;
 
-  const subHeadingIcon = document.createElement("img");
-  subHeadingIcon.src = "export.svg";
-  subHeadingIcon.alt = "export.svg-logo";
+    subHeadingContainer.appendChild(subHeadingIcon);
+    subHeadingContainer.appendChild(subHeading);
 
-  const subHeading = document.createElement("p");
-  subHeading.textContent = "15 % compared with last month";
+    cardHeadingContainer.appendChild(cardHeading);
+    cardHeadingContainer.appendChild(subHeadingContainer);
 
-  subHeadingContainer.appendChild(subHeadingIcon);
-  subHeadingContainer.appendChild(subHeading);
+    cardTop.appendChild(cardIconContainer);
+    cardTop.appendChild(cardHeadingContainer);
 
-  cardIconContainer.appendChild(cardIcon);
+    const line = document.createElement("div");
+    line.className = `line ${overviewCardItem.active ? "active" : ""}`;
 
-  cardHeadingContainer.appendChild(subHeadingContainer);
+    const cardBottom = document.createElement("div");
+    cardBottom.className = "card-bottom";
 
-  const line = document.createElement("div");
-  line.className = "line";
+    const balance = document.createElement("span");
+    balance.className = `balance ${overviewCardItem.active ? "active" : ""}`;
+    balance.textContent = `${overviewCardItem.balance}`;
 
-  const cardBottom = document.createElement("div");
-  cardBottom.className = "card-bottom";
+    const rightarrow = document.createElement("div");
+    rightarrow.className = `right-arrow ${
+      overviewCardItem.active ? "active" : ""
+    }`;
 
-  const balance = document.createElement("span");
-  balance.className = "balance";
-  balance.textContent = "$ 28,891.138";
+    cardBottom.appendChild(balance);
+    cardBottom.appendChild(rightarrow);
 
-  const rightarrow = document.createElement("img");
-  rightarrow.className = "right-arrow";
-  rightarrow.src = "arrow-right.svg";
-  rightarrow.alt = "arrow-right-logo";
+    cardContainer.appendChild(cardTop);
+    cardContainer.appendChild(line);
+    cardContainer.appendChild(cardBottom);
+    card.appendChild(cardContainer);
 
-  cardBottom.appendChild(balance);
-  cardBottom.appendChild(rightarrow);
+    return card;
+  }
 
-  cardContainer.appendChild(cardTop);
-  cardContainer.appendChild(line);
-  cardContainer.appendChild(cardBottom);
-
-  overViewCardContainer.appendChild(card);
+  for (let i = 0; i < overviewCardItems.length; i++) {
+    overViewCardContainer.appendChild(createCard(overviewCardItems[i]));
+  }
 
   overViewContainer.appendChild(overviewHeading);
   overViewContainer.appendChild(overViewCardContainer);
+
   return overViewContainer;
 }
